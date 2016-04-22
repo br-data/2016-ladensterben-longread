600e71cd08b8e839e6ce14d9c1bb25efc8010893
mzierer <Maximilian.Zierer@br.de>
2016-04-22 17:11:26 +0200

    Add Dorfladen logic

--- src/scripts/text.js
+++ src/scripts/text.js
@@ -16,14 +16,14 @@ var text = (function() {
       'nameKrz': 'Wunsiedel',
       'type': 'Landkreis',
       'regbez': 'Oberfranken',
-      'shopChgAbs': '-4',
+      'shopChgAbs': '-45',
       'shopChgPrc': '-45.6',
-      'shops05': '100',
-      'shops15': '76',
+      'shops05': '504',
+      'shops15': '680',
       'gemeindeMax': 'Unterneukirchen',
-      'nogroceryCount': '5',
-      'noshopCount': '4',
-      'popChange': '-11.35',
+      'nogroceryCount': '0',
+      'noshopCount': '0',
+      'popChange': '11.35',
       'brand': 'Edeka Nord',
       'space05': '470',
       'space15': '580',
@@ -47,6 +47,10 @@ var text = (function() {
     return array[dig] || dig;
   }
 
+  function getShortName(data) {
+    return data.nameKrz || data.name;
+  }
+
   function getWrittenPcg(pcg) {
 
     var result = '';
@@ -55,21 +59,21 @@ var text = (function() {
     if (pcg > 5 && pcg < 10) {
       result = 'leicht'
     } else if (pcg > 18 && pcg < 22) {
-      result = 'etwa ein Fünftel ';
+      result = 'etwa ein Fünftel';
     } else if (pcg > 22.1 && pcg < 27) {
-      result = 'etwa ein Viertel ';
+      result = 'etwa ein Viertel';
     } else if (pcg > 29 && pcg < 33.3) {
-      result = 'etwa ein Drittel ';
+      result = 'etwa ein Drittel';
     } else if (pcg >= 33.3 && pcg < 45 ) {
-      result = 'mehr als ein Drittel ';
+      result = 'mehr als ein Drittel';
     } else if (pcg > 45 && pcg < 50 ) {
-      result = 'etwa die Hälfte ';
+      result = 'etwa die Hälfte';
     } else if (pcg >= 50 && pcg < 57  ) {
-      result = 'mehr als die Hälfte ';
+      result = 'mehr als die Hälfte';
     } else if (pcg >= 60 && pcg < 66.6  ) {
-      result = 'etwa zwei Drittel ';
+      result = 'etwa zwei Drittel';
     } else if (pcg >= 66.6 ) {
-      result = 'mehr als zwei Drittel ';
+      result = 'mehr als zwei Drittel';
     } else {
       result = Math.round(pcg) + ' Prozent '
     }
@@ -83,57 +87,61 @@ var text = (function() {
     var paragraph = '';
     var ort = '';
     var prefix = '';
-    var menge = '';
-
-    
+    var menge = ''; 
 
     if (data.regbez === 'Oberpfalz') {
-      prefix = 'der ';
+      prefix = ' der';
     }
 
     // Landkreis oder Stadt?
     if (data.type === 'Landkreis') {
 
       paragraph += 'Im Landkreis ' + data.name + ' in ' + prefix + data.regbez + ' ';
-      wo = 'im Landkreis ';
+      wo = ' im Landkreis';
+      was = 'der Landkreis'
       headline += 'Landkreis ' + data.name + ': ';
     } else if (data.type === 'Stadt') {
 
       paragraph += 'In ' + data.name + ' ';
       wo = 'in der Stadt ';
-      headline += data.name + ': '
+      was = 'die Stadt'
+      headline += data.name + ':'
     }
 
     // Rückgang oder Anstieg
     if (data.shopChgPrc < 0) {
 
-      paragraph += 'ist der Einzelhandel in den vergangenen zehn Jahren um ' + getWrittenPcg(data.shopChgPrc) + ' zurückgegangen. Heute gibt es ' + wo + getDigitStr(-data.shopChgAbs) + ' Geschäfte weniger als noch im Jahr 2005. ';
-      headline += 'Einzelhandel geht zurück';
+      paragraph += ' ist der Einzelhandel in den vergangenen zehn Jahren um ' + getWrittenPcg(data.shopChgPrc) + ' zurückgegangen. Heute gibt es ' + wo + ' ' + getDigitStr(-data.shopChgAbs) + ' Geschäfte weniger als noch im Jahr 2005.';
+      headline += ' Einzelhandel geht zurück';
       if (data.spaceChgPrc > 0) {
-      paragraph += 'Gleichzeitig hat die durchschnittliche Verkaufsfläche der Geschäfte ' + wo + data.nameKrz + ' um ' + getWrittenPcg(data.spaceChgPrc) + 'zugenommen. Das deutet darauf hin, dass kleinere Läden verschwunden sind, während sich größere Märkte gehalten oder sogar neu eröffnet haben. ';     
+      paragraph += ' Gleichzeitig hat die durchschnittliche Verkaufsfläche der Geschäfte ' + wo + ' ' + getShortName(data) + ' um ' + getWrittenPcg(data.spaceChgPrc) + ' zugenommen. Das deutet darauf hin, dass kleinere Läden verschwunden sind, während sich größere Märkte gehalten oder sogar neu eröffnet haben.';     
       } else if (data.spaceChgPrc < 0) {
-      paragraph += 'Gleichzeitig hat auch die durchschnittliche Verkaufsfläche der Geschäfte ' + wo + data.nameKrz + ' abgenommen. Das deutet darauf hin, dass größere Märkte geschlossen haben, während sich kleinere Läden gehalten haben.';
+      paragraph += ' Gleichzeitig hat auch die durchschnittliche Verkaufsfläche der Geschäfte ' + wo + ' ' + data.nameKrz + ' abgenommen. Das deutet darauf hin, dass größere Märkte geschlossen haben, während sich kleinere Läden gehalten haben.';
       }
     } else if (data.shopChgPrc > 0) {
 
-      paragraph += 'ist der Einzelhandel in den vergangenen zehn Jahren um ' + getWrittenPcg(data.shopChgPrc) + ' gewachsen. ';
-      headline += 'Einzelhandel wächst';
+      paragraph += 'ist der Einzelhandel in den vergangenen zehn Jahren um ' + getWrittenPcg(data.shopChgPrc) + ' gewachsen.';
+      headline += ' Einzelhandel wächst';
     }
 
     if (data.noshopCount > 0) {
       paragraph += ''
     }
 
+    paragraph += ' Die Zahlen beruhen auf Erhebungen der Staatsregierung.';
 
+    if (data.dorfladen == 1) {
 
-    paragraph += 'Die Zahlen beruhen auf Erhebungen der Staatsregierung. ';
+      paragraph += ' Es gibt einen Dorfladen' + wo + ', in ' + data.dl1 + '.';
+    } else if (data.dorfladen == 2) {
 
-    if (data.dorfladen === 1) {
+      paragraph += ' Es gibt zwei Dorfläden' + wo + ' ' + getShortName(data) + ': In ' + data.dl1 + ' und ' + data.dl2 +'.';
+    } else if (data.dorfladen == 3) {
 
-      paragraph += 'Es gibt einen Dorfladen ' + wo + ' ' + data.nameKrz ;
-    } else if (data.dorfladen > 1) {
+      paragraph += ' Es gibt drei Dorfläden' + wo + ' ' + getShortName(data) + ': In ' + data.dl1 + ', ' + data.dl2 + ' und ' + data.dl3 +'.';
+    } else if (data.dorfladen > 3) {
 
-      paragraph += 'Es gibt ' + getDigitStr(data.dorfladen) + ' Dorfläden ' + wo + ' ' + data.nameKrz + '.';
+      paragraph += ' Es gibt' + getDigitStr(data.dorfladen) + ' Dorfläden' + wo + ' ' + getShortName(data) + '. Der Landkreis nimmt damit in Bayern eine Vorreiterrolle ein.';
     }
 
     return '<h3>' + headline + '</h3> <p>' + paragraph + '</p>';
