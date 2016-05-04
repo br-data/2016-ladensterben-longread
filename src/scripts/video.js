35dbfdec5cb624159b35bf1c48e59eb631086b2d
Steffen Kühne <stekhn@gmail.com>
2016-05-04 11:29:57 +0200

    Add mobile intro video fallback

--- src/scripts/video.js
+++ src/scripts/video.js
@@ -1,11 +1,18 @@
 var video = (function () {
 
+  // Get device width
+  var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
+
   function init() {
 
     videojs('intro-video', {}, function () {
 
       this.volume(0);
-      this.play();
+
+      if (width > 560) {
+
+        this.play();
+      }
     });
   }
 
